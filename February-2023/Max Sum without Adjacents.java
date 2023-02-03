ffd68667aed10ff75fb35eba32b123e8cddc329c
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2023-02-04 01:27:48 +0530

    Create Max Sum without Adjacents.java

---
+++ February-2023/Max Sum without Adjacents.java
@@ -0,0 +1,16 @@
+
+
+//User function Template for Java
+
+class Solution {
+    int findMaxSum(int arr[], int n) {
+        // code here
+        if(n==1) return arr[0];
+        int[] dp = new int[n+1];
+        dp[1]= arr[0];
+        for (int i=2;i<=n;i++) {
+            dp[i] = Math.max(dp[i-1], dp[i-2]+arr[i-1]);
+        }
+        return Math.max(dp[n],dp[n-1]);
+    }
+}
