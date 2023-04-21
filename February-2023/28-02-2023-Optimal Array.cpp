9cd38fb67d0cc2a8f7d31544c1fce9879cea46c2
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2023-04-21 10:34:41 +0530

    Create 28-02-2023-Optimal Array.cpp

---
+++ February-2023/28-02-2023-Optimal Array.cpp
@@ -0,0 +1,21 @@
+//User function Template for C++
+
+class Solution{
+public:
+    vector<int> optimalArray(int n,vector<int> &a){
+        // Code here
+        vector<int> ans(a.size(),0) ; 
+
+       for(int i=1 ; i<a.size() ; i++){            
+
+                    int index = (0 + i)/2;
+
+                    int val = (a[i] - a[index]) + ans[i-1] ;        
+
+                    ans[i] = val;
+
+        }     
+
+        return ans ;
+    }
+};
