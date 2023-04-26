922495ecde4234282fdcfb89606ee5b7290661ae
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2023-04-26 14:28:59 +0530

    Create 26-04-2023-Seating Arrangement.cpp

---
+++ April-2023/26-04-2023-Seating Arrangement.cpp
@@ -0,0 +1,25 @@
+//User function Template for C++
+
+class Solution{
+    public:
+    bool is_possible_to_get_seats(int n, int m, vector<int>& seats){
+        // Write your code here.
+        for(int i=0;i<m;i++){
+            if(n==0)return true;
+            if(i==0 && seats[i]==0 && seats[i+1]==0){
+                n--;
+                seats[i]=1;
+            }
+            else if(i==m-1 && seats[i]==0 && seats[i-1]==0){
+                n--;
+                seats[i]=1;
+            }
+            else if(seats[i]==0 && seats[i-1]==0 && seats[i+1]==0){
+                n--;
+                seats[i]=1;
+            }
+        }
+        if(n==0)return true;
+        return false;
+    }
+};
